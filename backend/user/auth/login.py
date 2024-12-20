aebd918da6903259d779583fd232b98da1e50fa7
c0ex38 <cagri.ozay@ogrenci.karatay.edu.tr>
2024-12-20 20:01:55 +0300

    API'lerin dizaynı

--- backend/user/auth/login.py
+++ backend/user/auth/login.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class LoginUserAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         data = request.data
